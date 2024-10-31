af5aeb9e4b927e8aa1ef2bd42ce2f36580599428
zzgh06 <zzgh06@naver.com>
2024-10-31 16:47:10 +0900

    feat : shared header layout components

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,5 +1,15 @@
 export default function Header(){
   return (
-    <div>Header</div>
+    <header className="bg-white shadow">
+      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex">
+            <div className="flex-shrink-0 flex items-center">
+              <h1 className="text-xl font-bold">Stock Portfolio</h1>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
   )
 }
